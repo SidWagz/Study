1152dc316c3b484a1e0ab74935f72ac18162d969
Sidwagz <siddharthwagle1@gmail.com>
2017-04-13 00:34:32 +0530

    Adding delete for singly

--- DS/singly.c
+++ DS/singly.c
@@ -30,8 +30,10 @@ Node* new_node(int n) {
 
 //All operations on singly list
 void print_list();
+int get_val(Node*);
 int list_length();
 void insert_node(int);
+void delete_node(int);
 
 //Print the list chain
 void print_list() {
@@ -43,6 +45,11 @@ void print_list() {
 		current = current->next;
 	}
 	printf("\n\n");
+}
+
+//Get value from Node
+int get_val(Node* node) {
+	return *(int*)(node->val);
 }
 
 //Returns length of list
@@ -75,11 +82,62 @@ void insert_node(int n) {
 	current->next = newnode;
 }
 
+//Delete a node from list
+void delete_node(int n) {
+
+	Node* prev = NULL, *current = ROOT;
+	while (current->next != NULL 
+				&& get_val(current) != n) {
+		prev = current;
+		current = current->next;
+	}
+
+	if (current == NULL)
+		printf("Cannot find node %d - List is empty\n\n", n);
+	
+	else if (get_val(current) != n)
+		printf("Cannot find node %d - Not in list\n\n", n);
+	
+	else {
+		Node* t = current;
+		if (prev == NULL) {
+			ROOT = current->next;
+		}
+		else {
+			prev->next = current->next;
+		}
+		free(t);
+	}
+}
+
 int main() {
 
+	//Create list and print it
 	insert_node(1);
 	insert_node(2);
+	insert_node(3);
+	insert_node(4);
 
+	printf("Length of list is %d\n\n", list_length());
+	print_list();
+
+	//Delete mid element
+	delete_node(2);
+	printf("Length of list is %d\n\n", list_length());
+	print_list();
+
+	//Delete root element
+	delete_node(1);
+	printf("Length of list is %d\n\n", list_length());
+	print_list();
+
+	//Delete last element
+	delete_node(4);
+	printf("Length of list is %d\n\n", list_length());
+	print_list();
+
+	//Delete element not in list
+	delete_node(5);
 	printf("Length of list is %d\n\n", list_length());
 	print_list();
 
